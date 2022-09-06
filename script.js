6f216ba9ea80710afeb10a08ba773e5d704bf2e7
Oksanka25 <chekoksa25@gmail.com>
2022-09-06 15:57:01 -0400

    Creted DOM variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// DOM variables
+let board = document.getElementById('board');
+let square = document.getElementsByClassName('square');
+let yellowChecker = document.getElementsByClassName('yellow_checker');
+let blueChecker = document.getElementsByClassName('blue_checker')
+
+
+// Game variables
